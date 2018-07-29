248f5049caf8da6e2abe94ceed14ac3fc7198906
Shuman Fang <shuman.fang@gmail.com>
2018-07-29 12:05:00 -0400

    add header

--- product.py
+++ product.py
@@ -16,3 +16,13 @@ products[0][0]
 
 for p in products:
 	print(p[0], 's price is', p[1])
+
+# 'abc' + '123' = 'abc123'
+# 'abc' * 3 = 'abcabcabc'
+
+with open('prodcuts.csv','w', encoding = 'utf-8') as f: 
+	#the encoding part is for the program to recognize as many languages as possible
+	#therefore it can read chinese if you have chinese as the title, if you only have english then dont really need it
+	f.write('product name, price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
