26906710a6966563aef5cfb88f1fd1656053e78c
Shuman Fang <shuman.fang@gmail.com>
2018-07-31 22:43:38 -0400

    full code

--- product.py
+++ product.py
@@ -1,4 +1,34 @@
+import os #import operating system
+
 products = []
+if os.path.isfile('products.csv') #check if there is already a file
+	print('yeah, found file')
+	#read file
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if 'product, price' in line:
+				continue #this will skip the following content in the loop and move on to the next loop
+			name, price = line.strip().split(',')
+			prodcuts.append9([name,price])
+	print(products)
+
+else:
+	print('cant find file')
+
+
+#read file
+# products = []
+# with open('products.csv', 'r', encoding='utf-8') as f:
+# 	for line in f:
+# 		if 'product, price' in line:
+# 			continue #this will skip the following content in the loop and move on to the next loop
+# 		name, price = line.strip().split(',')
+# 		prodcuts.append9([name,price])
+# print(products)
+
+
+
+#enter info
 while True:
 	name = input('please enter the product name: ')
 	if name == 'q': #quick
@@ -20,6 +50,8 @@ for p in products:
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
+
+#create file
 with open('prodcuts.csv','w', encoding = 'utf-8') as f: 
 	#the encoding part is for the program to recognize as many languages as possible
 	#therefore it can read chinese if you have chinese as the title, if you only have english then dont really need it
